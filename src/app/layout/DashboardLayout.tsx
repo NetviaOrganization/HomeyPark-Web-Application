0ad3e7b91a311930ce088869fcafe041fcf71b18
SgtGarro <marcelogarro137@gmail.com>
2025-04-23 04:31:07 -0500

    feat: add footer app and copyrights

--- src/app/layout/DashboardLayout.tsx
+++ src/app/layout/DashboardLayout.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router'
 import Sidebar from '../shared/components/Sidebar'
+import Footer from '../shared/components/Footer'
 
 const DashboardLayout = () => {
   return (
-    <div className="w-full h-screen flex">
-      <Sidebar />
-      <Outlet />
+    <div className="w-full h-screen flex flex-col">
+      <div className="w-full h-full flex">
+        <Sidebar />
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   )
 }
